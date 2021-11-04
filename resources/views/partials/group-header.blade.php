4366590409e16b88724f5c06a70a85eadc15a7d5
John Puddephatt <john@jdp.org.uk>
2021-11-04 23:46:19 +0000

    member contact

--- resources/views/partials/group-header.blade.php
+++ resources/views/partials/group-header.blade.php
@@ -66,7 +66,8 @@
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
         </svg>
-        <a target="_blank" href="{{ $contact_details->website }}">Website: {{ $contact_details->website }}</a>
+        <a target="_blank" href="{{ $contact_details->website }}">Website: {{ str_replace('http://', '',
+          str_replace('https://', '', $contact_details->website)) }}</a>
       </p>
       @endif
 
