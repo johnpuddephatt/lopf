2eca209c7cee5f1fc374d554070fb9dac02bc689
John Puddephatt <john@jdp.org.uk>
2021-11-09 23:37:30 +0000

    fix hero on md screens

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,6 +1,6 @@
-<div class="inline-block w-48 h-48 overflow-hidden lg:w-72 lg:h-72">
+<div class="inline-block w-48 h-48 overflow-hidden xl:w-72 xl:h-72">
     <div
-        class="antialiased text-left relative flex items-center pl-16 lg:pl-20 pr-16 mr-2 text-2xl lg:text-4xl font-bold rounded-full w-60 h-60 lg:w-80 lg:h-80 -left-12 lg:-left-12 lg:-top-12 -top-12 text-{{ $text ?? 'sky'}} bg-{{ $background ?? 'blue' }}">
+        class="antialiased text-left relative flex items-center pl-16 xl:pl-20 pr-16 mr-2 text-2xl xl:text-4xl font-bold rounded-full w-60 h-60 xl:w-80 xl:h-80 -left-12 xl:-left-12 xl:-top-12 -top-12 text-{{ $text ?? 'sky'}} bg-{{ $background ?? 'blue' }}">
         <div class="mt-4 leading-tight">
             {{ $siteName }}
         </div>
--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -10,7 +10,7 @@ Template Name: Home
     <div
       class="container z-10 flex flex-col-reverse items-center pr-0 mx-auto max-w-7xl min-h-header-sm lg:min-h-header md:flex-row">
 
-      <div class="max-w-4xl pb-24 md:flex-1 lg:pb-0">
+      <div class="max-w-4xl pb-24 md:flex-1 md:pb-0">
 
         <h2 class="text-4xl font-bold leading-none xl:text-6xl lg:leading-none lg:text-5xl text-purple">
           {!! $hero_title
@@ -21,7 +21,7 @@ Template Name: Home
 
       </div>
 
-      <div class="relative flex-none w-1/2 mb-20 ml-auto -mr-8 lg:my-0 lg:pl-40 ">
+      <div class="relative flex-none w-1/2 mb-20 ml-auto -mr-8 lg:my-0 md:pl-40 ">
         <div class="overlay">
           {!! $hero_image !!}
         </div>
