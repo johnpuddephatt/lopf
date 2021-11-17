352645cd394a5754b47c377b7ee71911045642c4
John Puddephatt <john@jdp.org.uk>
2021-11-17 15:34:23 +0000

    post card

--- resources/views/partials/post-card.blade.php
+++ resources/views/partials/post-card.blade.php
@@ -1,7 +1,7 @@
 <a href="{{ get_permalink($post_id ?? null) }}" @php(post_class('flex items-center flex-row body-font max-w-3xl'))>
     @if(!isset($hide_circles) || !$hide_circles)
     @if(has_post_thumbnail($post_id ?? null))
-    {!! get_the_post_thumbnail($post_id ?? null, 'square', [ 'class' => 'object-cover object-center mr-12 rounded-full
+    {!! get_the_post_thumbnail($post_id ?? null, 'square', [ 'class' => 'object-cover object-center mr-8 rounded-full
     w-36 h-36
     hidden lg:block flex-none' ])
     !!}
@@ -9,7 +9,7 @@
     <div class="flex-none hidden mr-12 rounded-full lg:block h-36 w-36 bg-orange-lightest"></div>
     @endif
     @endif
-    <div class="pl-6 border-l-8 border-orange">
+    <div class="pl-8 border-l-8 border-orange">
         <div class="mb-1 font-semibold text-blue ">{{ get_the_date(null, $post_id ?? null) }}</div>
         <h3 class="mt-0 mb-2 text-2xl font-bold">
             {!! get_the_title($post_id ?? null) !!}
