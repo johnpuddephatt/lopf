15cc1a6f3f28ca3ea6f3ff1ca75a1842026ab5de
John Puddephatt <john@jdp.org.uk>
2022-01-19 13:48:18 +0000

    fix project resource card

--- resources/views/partials/project-resource-card.blade.php
+++ resources/views/partials/project-resource-card.blade.php
@@ -1,13 +1,30 @@
-<a href="{{ $resource->link }}" @php(post_class('ring-1 flex items-center flex-row justify-between body-font mb-4
-    md:mb-8 p-4 lg:p-8 2xl:p-12 max-w-3xl border-l-8 ring-gray-200 border-pink '))>
+<a href="{{ get_permalink($resource->ID) }}" @php(post_class('ring-1 flex items-center flex-row justify-between
+    body-font bg-white max-w-3xl p-4 lg:p-8 2xl:p-10 border-l-8 ring-gray-200 border-pink '))>
+    
     <div class="max-w-3xl mr-8">
-        <h3 class="mt-0 mb-1 text-xl font-bold body-font">
+        
+        <h3 class="mt-0 mb-1 text-lg font-bold lg:text-xl text-blue body-font">
             {!! $resource->post_title !!}
         </h3>
-        @if(get_field(' date')) <div class="hidden md:block">
-    {{ get_field( 'date', $resource->ID ) }}
+
+        <div class="flex flex-col gap-4 mt-4 md:flex-row">
+        
+        @if(get_field(' date', $resource->ID))
+    <div class="text-gray-600">
+        {{ get_field('date', $resource->ID ) }}
+    </div>
+    @endif
+
+    @if(get_field("publisher", $resource->ID) && get_field("date", $resource->ID))
+    <span class="hidden text-gray-300 md:block">&mdash;</span>
+    @endif
+
+    @if(get_field("publisher", $resource->ID))
+    <div>
+        {{ get_field( "publisher", $resource->ID ) }}
     </div>
     @endif
     </div>
-    <x-icon.arrow-right class="w-8 h-12 text-pink" />
+    </div>
+    <x-icon.arrow-right class="flex-none w-8 h-12 text-pink" />
 </a>
