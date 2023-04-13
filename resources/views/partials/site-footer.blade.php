beeb8b2d03b788052c2dd0e26b1b93e3e4e81f08
John Puddephatt <john@jdp.org.uk>
2023-04-13 16:28:19 +0100

    nudge

--- resources/views/partials/site-footer.blade.php
+++ resources/views/partials/site-footer.blade.php
@@ -2,12 +2,12 @@
   <div class="antialiased text-gray-100 bg-blue">
     <div class="container pt-24 pb-16 ">
       <div
-        class="flex flex-wrap order-first space-y-8 text-center md:space-x-4 2xl:space-x-8 md:space-y-0 md:text-left">
+        class="flex flex-wrap order-first space-y-8 text-center space-x-4 2xl:space-x-8 md:space-y-0 md:text-left">
         <div class="flex-shrink flex flex-row justify-center md:justify-start w-full gap-4 mt-3 text-center md:text-left lg:w-1/4 mr-auto md:w-1/2">
           <x-logo :large="false" text="blue" background="white" />
           <x-forum-central-logo />
         </div>
-        <div class="flex-shrink w-full mr-auto lg:pt-4 md:w-2/3 lg:w-1/3 !lg:mr-auto">
+        <div class="flex-shrink w-full mr-auto lg:pt-4  lg:w-1/3 !lg:mr-auto">
           @if(get_theme_mod('contact_address'))
           <p class="mb-4 text-base site-footer--address contact-address">{!!
             nl2br(strip_tags(get_theme_mod('contact_address')))
